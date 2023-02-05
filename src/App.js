aa323331aed76e2fe01b1e4b1183a86bec738e76
Leon <leon@leons-air.home>
2023-02-05 14:36:14 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [height, setHeight] = useState('')
+  const [weight, setWeight] = useState('')
+  const [bmi, setBmi] = useState('')
+
+function bmiCalc(){
+  if (!weight || !height){
+    return
+  } else {
+    setBmi(
+      weight/(height*height)
+      )
+  }
+  }
+
+function reload(){
+    setBmi('')
+    setHeight('')
+    setWeight('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='bmi-tracker'>
+      <div className='title'>
+        <h1>BMI Tracker</h1>
+      </div>
+      <div className='input-container'>
+        Height (M)
+        <input type="text" placeholder='Height(M)' value={height} id="height" onChange={(e)=>setHeight(e.target.value)}/>
+        Weight (KG)
+        <input type="text" placeholder='Weight(KG)' value={weight} id="weight" onChange={(e)=>setWeight(e.target.value)}/>
+      </div>
+      <div className='button-container'>
+        <button onClick={bmiCalc}>Submit</button>
+        <button onClick={reload}>Reload</button>
+        </div>
+      <div className="result">
+        Your BMI is: {bmi}
+      </div>
+      </div>
     </div>
   );
 }
